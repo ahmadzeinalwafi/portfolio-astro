1b6dd98928ac84f7ff681465d836d93f46e3569c
Ahmad Zein Al Wafi <drakeequehours@gmail.com>
2025-03-19 12:27:04 +0700

    add: components

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+// tailwind.config.cjs
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // or 'media' for system preference
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
